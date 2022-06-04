4c988c80287232e51be55ae0409a2c6a05879e06
mirik070 <75161328+mirik070@users.noreply.github.com>
2022-06-04 23:18:36 +0500

    bug fix

--- eco_nur/settings.py
+++ eco_nur/settings.py
@@ -34,7 +34,7 @@ LOGIN_REQUIRED_URLS_EXCEPTIONS = (
     r'/admin(.*)$',
     '/accounts/login/',
 )
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '85.143.175.216']
 
 # Application definition
 
