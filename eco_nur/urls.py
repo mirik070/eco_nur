2ba74a3d8a6919e9fe68afbec0e68ebdf3f62b39
mirik070 <75161328+mirik070@users.noreply.github.com>
2022-06-09 12:55:11 +0500

    static bug fix

--- eco_nur/urls.py
+++ eco_nur/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from eco_nur import settings
 
@@ -28,4 +29,5 @@ urlpatterns = [
     path('', include('user.urls')),
     path('', include('warehouse.urls')),
 ]
+urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
